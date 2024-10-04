f8dee16e1e6a5c2ab2e7673c3f89a1d9b5d4d875
DESKTOP-5ULSK89\CAROL <carolmonroy54@gmail.com>
2024-10-04 09:31:34 -0600

    Se agrega la pantalla Login en su versión móvil

--- LenayGG-Movil/App.xaml.cs
+++ LenayGG-Movil/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new LenayGG_Movil.Views.LoginMovil();
         }
     }
 }
---
+++ LenayGG-Movil/Views/LoginMovil.xaml.cs
@@ -0,0 +1,9 @@
+namespace LenayGG_Movil.Views;
+
+public partial class LoginMovil : ContentPage
+{
+	public LoginMovil()
+	{
+		InitializeComponent();
+	}
+}

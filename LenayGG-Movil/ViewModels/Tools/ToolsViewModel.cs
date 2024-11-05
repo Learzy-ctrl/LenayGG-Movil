bf5e77696d8a2263f01759bdf8aa7bfa2ab4cbe6
Israel Curiel <israelcuriel597@hotmail.com>
2024-11-05 12:38:27 -0600

    LGG: Creacion de viewmodel

---
+++ LenayGG-Movil/ViewModels/Tools/ToolsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Acr.UserDialogs;
+using CommunityToolkit.Mvvm.Input;
+using LenayGG_Movil.Views.Tools.Account;
+using System.Windows.Input;
+
+namespace LenayGG_Movil.ViewModels.Tools
+{
+    public class ToolsViewModel
+    {
+        public INavigation _navigation { get; set; }
+        private IServiceProvider _serviceProvider;
+        public ToolsViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            GoToAccountCommand = new AsyncRelayCommand(GoToAccount);
+        }
+
+        #region Methods
+        private async Task GoToAccount()
+        {
+            var userPage = _serviceProvider.GetService<UserPage>();
+            UserDialogs.Instance.ShowLoading("Cargando");
+            await _navigation.PushModalAsync(userPage);
+            UserDialogs.Instance.HideLoading();
+        }
+        #endregion
+
+        #region Commands
+        public ICommand GoToAccountCommand { get; private set; }
+        #endregion
+    }
+}
